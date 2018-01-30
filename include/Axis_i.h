2a7a0b856fcb1038b73c70f760d170c6402db00a
Nate <nate@useclark.com>
2018-01-30 11:52:15 -0500

    01-30-2018

--- include/Axis_i.h
+++ include/Axis_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Tue Jan 30 10:26:53 2018
+/* at Tue Jan 30 11:19:22 2018
  */
 /* Compiler settings for Axis.odl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
